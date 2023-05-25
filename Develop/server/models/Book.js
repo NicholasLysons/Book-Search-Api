4af8c3b4d9191d274f22637e928dac8d8db3021b
Nicholas Davis Lysons <stmpkjunkie@Nicholass-MacBook-Air.local>
2023-05-25 16:06:32 -0700

    fixed models for better quality

--- Develop/server/models/Book.js
+++ Develop/server/models/Book.js
@@ -1,7 +1,12 @@
-const { Schema, model} = require('mongoose');
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
+  
+  bookId: {
+    type: String,
+    required: true,
+  },
   authors: [
     {
       type: String,
@@ -11,8 +16,7 @@ const bookSchema = new Schema({
     type: String,
     required: true,
   },
-  // saved book id from GoogleBooks
-  bookId: {
+  title: {
     type: String,
     required: true,
   },
@@ -22,12 +26,6 @@ const bookSchema = new Schema({
   link: {
     type: String,
   },
-  title: {
-    type: String,
-    required: true,
-  },
-});
+  });
 
-const Book = model('Book', bookSchema);
-
-module.exports = Book;
+module.exports = bookSchema;
